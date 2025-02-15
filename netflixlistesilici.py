a068d057e6301d88eef5cfdc6d6577312a525297
Um4t <umutyedek444@gmail.com>
2025-02-16 00:04:16 +0300

    Add files via upload

---
+++ netflixlistesilici.py
@@ -0,0 +1,14 @@
+import os
+
+# Silinecek dosyanın yolu
+dosya_yolu = "/storage/emulated/0/um4tnetflixhits.txt"  # Dosyanın tam yolunu yazabilirsin: örn. "C:/Users/Kullanici/Desktop/sil.txt"
+
+def dosya_sil():
+    if os.path.exists(dosya_yolu):
+        os.remove(dosya_yolu)
+        print(f"✅ {dosya_yolu} başarıyla silindi.")
+    else:
+        print("❌ Dosya bulunamadı!")
+
+# Çalıştır
+dosya_sil()
